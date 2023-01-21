4f5bb94e39e28cc6e9758329f4f40dbe2d99d219
JonasBechler <jonasbechler@gmail.com>
2023-01-22 00:33:15 +0100

    bugfix

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ const path = require('path');
 
 const config = require( "../../config.json");
 
-
 config.port = config.konrad.port
 config.port_react = config.konrad.port_react
 config.session_secret = config.konrad.session_secret
 config.fusionauth = config.konrad.fusionauth
 
-const frontend_dir = path.join(__dirname, "..", "..", "SWK_Konrad_React", "build")
+const frontend_dir = path.join(__dirname, "..", "SWK_Konrad_React", "build")
 const userDataPath = path.join(__dirname, "..", "data", "accounts.json")
 
 var app = require('./SWK_Server_Template/src/main')(config, userDataPath, frontend_dir)
